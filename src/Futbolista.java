09d32e2f69f22baa60285259b32996b0b0395db0
Ana Castillo <ancastill795@gmail.com>
2025-05-27 07:58:20 -0600

    Corregi las tablas

--- src/Futbolista.java
+++ src/Futbolista.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+public class Futbolista extends Persona{
+    private int dorsal;
+    private String demarcacion;
 
-public class Futbolista {
+    public Futbolista() {
+    }
+
+    public Futbolista(int id, String nombre, String apellidos, int edad, int dorsal, String demarcacion) {
+        super(id, nombre, apellidos, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El futboloista se concentra");
+    }
+
+    @Override
+    public void viajar() {
+        System.out.println("El futbolista viaja con su eqipo");
+    }
+
+    public void jugarpartido() {
+        System.out.println("El futbolista juega partidos");
+    }
+
+    public void entrenar() {
+        System.out.println("El futbolista entrena");
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+public abstract class Persona {
+    protected int id;
+    protected String nombre;
+    protected String apellidos;
+    protected int edad;
 
-public class Persona {
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String apellidos, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public  abstract void concentrarse();
+    public abstract  void viajar();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
 }
